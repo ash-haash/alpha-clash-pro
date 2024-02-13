2baf219dc9c72b45d7762ae7f77886c36015f221
ashrafur rahman farhad <itsfaarhaad@gmail.com>
2024-02-13 21:27:54 +0000

    another commit after adding some js functionality

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,18 @@
+function continueGame() {
+    // generate and get a random alphabet
+    const alphabet = getARandomAlphabet();
+
+    // set random alphabet to the playground screen
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color on randomly generated key
+    setBackgroundColorById(alphabet);
+}
+
+
 function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame();
 }
